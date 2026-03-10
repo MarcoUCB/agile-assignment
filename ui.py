a135ec6f08c44c4142cef5876e4c7be01a16ba5e
K0Stek122 <kamilianos3@gmail.com>
2026-03-10 15:30:16 +0000

    feat: added a button

--- ui.py
+++ ui.py
@@ -18,12 +18,12 @@ class BaseGUI(wx.Frame):
     def _EndGridBuild(self):
         self.panel.SetSizerAndFit(self.grid)
         frame_sizer = wx.BoxSizer(wx.VERTICAL)
-        frame_sizer.Add(self.panel, 0, wx.ALL, 15)
+        frame_sizer.Add(self.panel, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 15)
         
         self.SetSizerAndFit(frame_sizer)
         self.Centre()
 
-    def _AddCenteredText(self, text : str, pos : tuple[int, int], span : tuple[int, int] = (1, 1)):
+    def _AddText(self, text : str, pos : tuple[int, int], span : tuple[int, int] = (1, 1)):
         self.grid.Add(
             wx.StaticText(self.panel, label=text),
             pos=(pos[0], pos[1]),
@@ -31,14 +31,23 @@ class BaseGUI(wx.Frame):
             flag=wx.ALIGN_CENTER
         )
         
-    def _AddCenteredTextbox(self, pos : tuple[int, int], span : tuple[int, int] = (1, 1)):
+    def _AddTextbox(self, pos : tuple[int, int], span : tuple[int, int] = (1, 1)):
         self.grid.Add(
             wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER), 
             pos=(pos[0], pos[1]),
             span=(span[0], span[1]),
             flag=wx.ALIGN_CENTER
-        )
+        ) 
     
+    def _AddButton(self, text : str, pos : tuple[int, int], span : tuple[int, int] = (1, 1)) -> wx.Button:
+        button = wx.Button(self.panel, label=text)
+        self.grid.Add(
+                button,
+                pos=(pos[0], pos[1]),
+                span=(span[0], span[1]),
+                flag=wx.ALIGN_CENTER
+        )
+        return button
 
 
 # PARENT ROOT GUI
@@ -49,12 +58,20 @@ class WelcomeGUI(BaseGUI):
         
         self._StartGridBuild(4, 2)
         
-        self._AddCenteredText("Welcome to GymPro", (0, 0), (1, 2))
+        self._AddText("Welcome to GymPro", (0, 0), (1, 2))
         
-        self._AddCenteredText("Login: ", (1, 0))
-        self._AddCenteredTextbox((1, 1))
+        self._AddText("Login: ", (1, 0))
+        self._AddTextbox((1, 1))
         
-        self._AddCenteredText("Password: ", (2, 0))
-        self._AddCenteredTextbox((2, 1))
+        self._AddText("Password: ", (2, 0))
+        self._AddTextbox((2, 1))
+        
+        widget_login_button = self._AddButton("Login / Register", (3, 0), (1, 2))
+        widget_login_button.Bind(wx.EVT_BUTTON, self._onLoginButtonPress)
 
         self._EndGridBuild()
+    
+    def _onLoginButtonPress(self, event):
+        # Jamie: Implement user login / register
+        wx.MessageBox("Clicked!")
+        
