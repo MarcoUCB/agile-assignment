bef818fb4c240f59450fd2555082ebee877a4790
K0Stek122 <kamilianos3@gmail.com>
2026-03-10 09:47:59 +0000

    feat: implemented argument parsing

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import argparse
+
+def setup_arguments():
+    parser = argparse.ArgumentParser(prog="agile-assignment", description="App for gym membership management.")
+    
+    parser.add_argument('-c', '--cli', help="Run the app in CLI mode only.")
+    
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    print("Hello World")
+    args = setup_arguments()
+    
