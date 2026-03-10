7d116290764fb6828d08ee8cf3afb924c318fc3a
K0Stek122 <kamilianos3@gmail.com>
2026-03-10 10:22:16 +0000

    feat: implemeneted skeleton sqlite framework up for extension by Jamie.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import argparse
+from sqlite import SQLite
 
-def setup_arguments():
+db_conn = None
+
+def setup_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(prog="agile-assignment", description="App for gym membership management.")
     
-    parser.add_argument('-c', '--cli', help="Run the app in CLI mode only.")
+    parser.add_argument('mode', choices=['cli', 'gui'], help="Run the app in CLI or GUI mode.")
     
     return parser.parse_args()
 
+def setup_database():
+    db_conn = SQLite("db")
+    db_conn.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, Name TEXT)")
+
+def run_cli():
+    db_conn = SQLite("db")
+    db_conn.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, Name TEXT)")
+
 if __name__ == "__main__":
-    args = setup_arguments()
-    
+    args = setup_arguments() 
+    if args.mode == 'cli':
+        run_cli()
---
+++ sqlite.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class SQLite:
+    def __init__(self, db_name : str):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def execute(self, command : str):
+        self.cursor.execute(command)
+        return self.cursor.fetchall()
