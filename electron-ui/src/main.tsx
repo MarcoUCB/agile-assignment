66e6426fe27f1fd143522f5db1f2d169338fbac0
K0Stek122 <kamilianos3@gmail.com>
2026-03-24 08:29:54 +0000

    feat: Added both web app and desktop app support

--- electron-ui/src/main.tsx
+++ electron-ui/src/main.tsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>,
 )
 
-// Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
+// Only available when running inside Electron with preload bridge.
+window.ipcRenderer?.on('main-process-message', (_event, message) => {
   console.log(message)
 })
