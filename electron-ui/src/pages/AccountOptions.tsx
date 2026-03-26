9b01680fa835d49fb09e9825c409df7bbf51f205
K0Stek122 <kamilianos3@gmail.com>
2026-03-26 20:44:37 +0000

    fix: fixed typo in Dockerfiles. Fixed buttons in AccountOptions being out of bounds.

--- electron-ui/src/pages/AccountOptions.tsx
+++ electron-ui/src/pages/AccountOptions.tsx
@@ -5,8 +5,8 @@ function AccountOptions() {
     return (
         <SidebarWrapper title="Account Options">
             <div className="flex flex-col gap-4 p-8 login-card-enter">
-                <Button variant="default">Request your Data</Button>
-                <Button variant="default">Delete Account</Button>
+                <Button variant="default" className="w-1/4">Request your Data</Button>
+                <Button variant="default" className="w-1/4">Delete Account</Button>
             </div>
         </SidebarWrapper>
     )
